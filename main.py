031614c0dfb5f482112d40c5e2a0d7038178fdcf
GLRenderer <GLRenderer@users.noreply.github.com>
2021-08-18 09:21:00 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import fastapi
+import uvicorn
+
+api = fastapi.FastAPI()
+
+def main():
+    ''' Entry point for project '''
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
+    
